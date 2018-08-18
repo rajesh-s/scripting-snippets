2381da22d63933f8bfa0066283cfa32776248c05
hello-w0r1d <35628747+hello-w0r1d@users.noreply.github.com>
2018-08-18 22:42:13 +0530

    Update find_in_list.sh

--- bash/find_in_list.sh
+++ bash/find_in_list.sh
@@ -4,4 +4,8 @@
 while read j; do
   # body of code
   # eg: grep "\<$j>\" file >> file2
+  # Store the value assigned to a variable from a file in a bash variable eg. get the value 7 from a 7 in file1 to a var temp
+  # temp=$(grep 'a' file1 | awk '{print $2}')
+  # Print with formatting
+  # printf "%-20s \t | a: %d | b: %.1f \n" $a $b >> file2
 done < file_list
